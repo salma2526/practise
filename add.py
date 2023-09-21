54beb84b2e7c968acc7e520da5a67a7c4df4749a
salma2526 <104891275+salma2526@users.noreply.github.com>
2023-09-21 10:50:17 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,9 +1,15 @@
-# add_numbers.py
-def add(a, b):
-    return a + b
+# factorial.py
+import math
+
+def calculate_factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers"
+    elif n == 0:
+        return 1
+    else:
+        return math.factorial(n)
 
 if __name__ == "__main__":
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    result = add(num1, num2)
-    print(f"The sum of {num1} and {num2} is {result}")
+    num = int(input("Enter a number to calculate its factorial: "))
+    result = calculate_factorial(num)
+    print(f"The factorial of {num} is {result}")
